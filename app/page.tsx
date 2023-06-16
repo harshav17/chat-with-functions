434583db8a2d67ccf18e4184b5d9a078ce18c4f4
harsha <harshav17@gmail.com>
2023-06-15 17:29:59 -0700

    Redirect + Add AI SDK

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="mx-auto px-4 sm:px-6 lg:px-8 sticky top-0 z-50 bg-white">
         <div className="relative flex h-16 items-center justify-between border-b border-gray-200 ">
           <div className="flex items-center">
-            <a className="flex-shrink-0 text-2xl font-bold" href=".">
-              meet
+            <a className="flex-shrink-0 text-2xl font-bold" href="/chat">
+              Chat
             </a>
           </div>
         </div>
